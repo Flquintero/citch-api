c820f39d2b9d7f34e3f005252e35d3612d9841c7
Francisco Quintero <flquintero1@gmail.com>
2022-07-29 16:33:28 -0400

    added appcheck to backend

--- functions/src/api/auth/index.ts
+++ functions/src/api/auth/index.ts
@@ -1,10 +1,12 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import authService from '../../services/auth';
+import { $appCheckVerification } from '../../utils/app-check-verification';
 
 const authRouter = Router();
 
 authRouter.post(
   '/verify-password-code',
+  [$appCheckVerification],
   async (req: Request, res: Response, next: NextFunction) => {
     const passwordInfo = await authService.verifyPassword(req.body, next);
     res.send(passwordInfo);
@@ -13,6 +15,7 @@ authRouter.post(
 
 authRouter.post(
   '/confirm-password-reset',
+  [$appCheckVerification],
   async (req: Request, res: Response, next: NextFunction) => {
     const passwordInfo = await authService.confirmPasswordReset(req.body, next);
     res.send(passwordInfo);
--- functions/src/api/organizations/index.ts
+++ functions/src/api/organizations/index.ts
@@ -1,11 +1,16 @@
 import { Request, Response, NextFunction, Router } from 'express';
 import organizationsService from '../../services/organizations';
+import { $appCheckVerification } from '../../utils/app-check-verification';
 
 const organizationsRouter = Router();
 
-organizationsRouter.post('/update', async (req: Request, res: Response, next: NextFunction) => {
-  res.json(await organizationsService.update(req.body, next));
-});
+organizationsRouter.post(
+  '/update',
+  [$appCheckVerification],
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.json(await organizationsService.update(req.body, next));
+  }
+);
 
 organizationsRouter.get('*', async (req: Request, res: Response) => {
   res.status(404).send('This route does not exist.');
