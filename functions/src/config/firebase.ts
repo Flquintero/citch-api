59aee8880df0cf2e1f1ca98503a9d0f04cebf2eb
Francisco Quintero <flquintero1@gmail.com>
2022-09-16 15:35:24 -0400

    added doppler

--- functions/src/config/firebase.ts
+++ functions/src/config/firebase.ts
@@ -1,16 +1,20 @@
 import * as admin from 'firebase-admin';
 import * as functions from 'firebase-functions';
+const secrets = functions.config().doppler;
+
+// Uncomment below to test if secrets are being updated - can check in the google cloud console for the project and see the logs not the variables
+//console.log('secrets', secrets);
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    privateKey: process.env.APP_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
-    projectId: process.env.APP_FIREBASE_PROJECT_ID,
-    clientEmail: process.env.APP_FIREBASE_CLIENT_EMAIL,
+    privateKey: secrets.APP_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    projectId: secrets.APP_FIREBASE_PROJECT_ID,
+    clientEmail: secrets.APP_FIREBASE_CLIENT_EMAIL,
   }),
-  databaseURL: process.env.APP_FIREBASE_DATABASE_URL,
+  databaseURL: secrets.APP_FIREBASE_DATABASE_URL,
 });
 
 const FieldValue = admin.firestore.FieldValue;
 
 const db = admin.firestore();
-export { admin, db, functions, FieldValue };
+export { admin, db, functions, FieldValue, secrets };
